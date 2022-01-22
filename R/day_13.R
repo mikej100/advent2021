5d88f0adf54e1652f0d188b9200c5ebc224f392c
Mike Jennings <mike.jennings@chwd.uk>
2022-01-22 00:29:34 +0000

    Solved Day 13 task2. Needs a tidy to automate test.

--- R/day_13.R
+++ R/day_13.R
@@ -13,11 +13,11 @@ wrangle_dots <- function (raw) {
   return (list(coords = coords, folds = folds))
 }
 
-# Make the matrix to fold matrics in half along axos of row centre or 
+# Make the matrix to fold matrices in half along axis of row centre or 
 # column centre.
-# dim is dimension in the axis perpendicualr to the fold
-# axis is the axis of the fole taking value "row" or "col".
-# returns squere matrix, left multipy for row fold and right multiply for 
+# dim is dimension in the axis perpendicular to the fold
+# axis is the axis of the fold taking value "row" or "col".
+# returns square matrix, left multiply for row fold and right multiply for 
 # column fold
 make_folder <- function (dim, axis='row') {
   make_entries <- function (dim) {
@@ -45,7 +45,7 @@ count_dots <- function (m) {
   
 }
 
-dot_data <- wrangle_dots( day13_data )
+dot_data <- wrangle_dots( test_day13_data )
 
 m0 <- matrix(0, nrow=max(dot_data$coords$y)+1, ncol = max(dot_data$coords$x)+1 )
 
@@ -61,4 +61,12 @@ m2<- fold_by_col(m1)
 count_dots(m2)
 
 m3<- fold_by_col(m2)
+folds <- dot_data$folds
+folds
 
+is_v_fold <- map_lgl(folds, ~ str_detect( .x, fixed( "x")))
+is_v_fold
+
+aaa <- reduce(is_v_fold, .init=m1, .f={
+  function(m,b) ifelse(b, fold_by_col(m), fold_by_row(m))
+})
