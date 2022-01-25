8aab57c105a6ebfa4335373b611ee79622a14f0a
Mike Jennings <mike.jennings@chwd.uk>
2022-01-25 23:33:56 +0000

    Complete Day 14 task 1.

--- R/day_14.R
+++ R/day_14.R
@@ -1,6 +1,8 @@
 # Day 14
 library(purrr)
 library(dplyr)
+library(stringr)
+library(readr)
 library(usethis)
 
 wrangle_polymer_data <- function(raw_data) {
@@ -17,13 +19,6 @@ wrangle_polymer_data <- function(raw_data) {
   
   return (list(template=template, pair_rules = pair_rules))
 }
-
-raw_data <- test_day14_data
-polymer_data <- wrangle_polymer_data(raw_data)
-# lookup table for pair rules to insert value.
-lk <- polymer_data$pair_rules
-# polymer starting template.
-tp <- polymer_data$template
 
 # calculate letters to insert into template tp for lookup table lk
 insert <- function(tp, lk) {
@@ -42,4 +37,20 @@ make_polymer <- function (tp, lk, n) {
   reduce(1:n, .init=tp, \(x, i) zip(x, insert(x, lk) ) )
 }
 
-make_polymer( tp, lk, 2 )
+get_polymer_metric <- function (raw_polymer_data) {
+  polymer_data <- wrangle_polymer_data(raw_polymer_data)
+  # lookup table for pair rules to insert value.
+  lk <- polymer_data$pair_rules
+  # polymer starting template.
+  tp <- polymer_data$template
+  
+  e_counts <- make_polymer(tp, lk, 10) |>
+    as_tibble() |>
+    count(value) |>
+    arrange(n)
+  
+  most_minus_least_e <- tail(e_counts$n, 1) - head(e_counts$n, 1)
+  
+  return (most_minus_least_e)
+} 
+ 
--- tests/testthat/test-day_14.R
+++ tests/testthat/test-day_14.R
@@ -1,3 +1,14 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(testthat)
+
+source("./R/day_13.R")
+
+test_that("Day14 task 1 polymer generation",{
+  
+  data_fname <- "day14_polymer.txt"
+  # day13_data <-  read_data(data_fname) 
+  test_day14_data <-  test_read_data(data_fname) 
+  day14_data <-  read_data(data_fname) 
+ 
+  expect_equal(get_polymer_metric(test_day14_data), 1588)
+  expect_equal(get_polymer_metric(day14_data), 2194)
 })
